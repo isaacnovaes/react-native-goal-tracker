d4753a8d2c4d4b596fc4f9017b2351d17166a7fa
Isaac <isaac.novaes@hotmail.com>
2022-08-15 20:15:09 +0200

    Update settings

--- colors.ts
+++ colors.ts
@@ -1,8 +1,10 @@
 const colors = {
     white: '#fff',
-    red: 'red',
-    borderColor: '#ccc',
+    red: '#f31282',
+    borderColor: '#e4d0ff',
     violet: '#5e0acc',
+    darkPurple: '#311b6b',
+    lightPurple: '#a065ec',
 };
 
 export default colors;
